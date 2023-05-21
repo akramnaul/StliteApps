ede83bd90b7d8a50c5facd8391122c213b8cd884
akramnaul <60735293+akramnaul@users.noreply.github.com>
2023-05-21 09:46:25 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 st.title("stlite sharing: Serverless Streamlit app platform")
 
-image = Image.open('stlitelogo.png')
-st.image(image, caption='stlite logo')
+#image = Image.open('stlitelogo.png')
+#st.image(image, caption='stlite logo')
 
 st.markdown("""
 ### stlite
